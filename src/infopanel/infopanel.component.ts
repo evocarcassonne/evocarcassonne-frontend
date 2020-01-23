dbf76dda447b32ef6036502c9e005971ff4546d3
ilkukrisztian <ilku355@gmail.com>
2020-01-23 15:32:58 +0100

    Removed unnecessary component

--- src/infopanel/infopanel.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, Injectable } from "@angular/core";
-
-@Component({
-  selector: "info-panel",
-  templateUrl: "./infopanel.component.html"
-})
-@Injectable()
-export class InfoPanelComponent {}
